f077d1d2988c7f34559a4aa5bcafc90ecef82a25
8eomhani <95764698+8eomhani@users.noreply.github.com>
2024-03-15 17:31:01 +0900

    Update 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기.sql

--- 프로그래머스/2/151137. 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기/자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기.sql
+++ 프로그래머스/2/151137. 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기/자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기.sql
@@ -6,3 +6,8 @@ OR OPTIONS LIKE '%열선시트%'
 OR OPTIONS LIKE '%가죽시트%'
 GROUP BY CAR_TYPE
 ORDER BY CAR_TYPE ASC;
+
+/*
+WHERE절에 IN을 쓰면 되지 않을까? 라는 생각을 했는데
+문제를 보면 알 수 듰듯이 CAR_TYPE컬럼의 레코드가 열선시트,가죽시트와 같이 여러개로 있었음 (제1정규형을 만족하지 않는 상태)
+그렇기 떄문에 WHERE절에 LIKE를 이용하여 문제를 해결하였음
